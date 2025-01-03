c4fc74da342144feff011de571fc32c368df9fab
Gandoler <gl.krutoi@mail.ru>
2025-01-03 20:34:58 +0300

    бля чет делаю а что делаю хуй его еби

--- Httpclient/DBTools/Data/DbContextSteam.cs
+++ Httpclient/DBTools/Data/DbContextSteam.cs
@@ -1,13 +1,36 @@
 using Dbtools.models;
+using Httpclient.AppSettings;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dbtools.Data;
 
 public class DbContextSteam: DbContext
 {
-    
-    
-    
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseMySql(ConnectionStringManager.GetConnectionString()
+        , ServerVersion.AutoDetect(ConnectionStringManager.GetConnectionString()));
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        
+        modelBuilder.Entity<SteamItem>(entity =>
+        {
+            entity.ToTable("SteamItems"); // Название таблицы
+            entity.Property(e => e.ItemType).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.ItemName).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Quality)
+                .IsRequired()
+                .HasConversion(
+                    v => v.ToString(), // Сохранение как строка
+                    v => (ItemQuality)Enum.Parse(typeof(ItemQuality), v)); // Чтение из строки
+  
+            
+        });
+    }
+
     public DbSet<SteamItem> Items { get; set; }
 }
